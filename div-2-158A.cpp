7a2dbacaab800ed40e642f1528427c139b42fa1b
Pranoy12001 <pranoy.mbstu12001@gmail.com>
2018-12-03 20:06:14 +0600

    Solved codeForces problem DIV-2(158-A) named Next Round

--- div-2-158A.cpp
+++ div-2-158A.cpp
@@ -4,18 +4,17 @@ using namespace std;
 
 int main()
 {
-    int n, k, a, count = 0;
+    int n, k, a[55], count = 0;
     cin>>n>>k;
 
     for(int i = 0;i < n; i++)
     {
-        cin>>a;
-        if(a > 0){
-           if(count > k){
-               break;
-           }else{
-               count++;
-           }
+        cin>>a[i];
+    }
+
+    for(int i= 0; i < n; i++){
+        if(a[i] >= a[k-1] && a[i] > 0){
+            count++;
         }
     }
     cout<<count<<endl;
